7dca5d9779c2a2a4c76a02a5099bbfe2a9f96097
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-09-21 17:37:43 +0100

    creating a layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+import Layout from './components/Layout/Layout';
+
+require('popper.js');
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout><p>Test</p></Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/auxiliary';
+
+const layout = (props) => {
+    return(
+        <Aux>
+            <div>Toolbar, SideDrawer, Backdrop</div>
+            <main>
+                {props.children}
+            </main>
+        </Aux>
+    )  
+}
+
+export default layout;
