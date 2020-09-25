a662321ac8e764c4e4c86ff011bd73b20282f9af
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-09-25 12:09:29 +0100

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/auxiliary';
 import Button from '../../UI/Button/Button';
 import './OrderSummary.scss';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return <li key={igKey} className="text-capitalize list">{igKey}: {props.ingredients[igKey]}</li>
-    });
-    return(
-        <Aux>
-            <h3>Your Order:</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul className="centre">
-                {ingredientSummary}
-            </ul>
-            <p>Total Price: £{props.price.toFixed(2)}</p>
-            <p>Continue to checkout?</p>
-            <div className="row">
-                <div className="col-md">
-                    <Button btnType="btn-danger" clicked={props.declinePurchase}> &#x2715; </Button>
-                </div>
-                <div className="col-md">
-                    <Button btnType="btn-success" clicked={props.acceptPurchase}> &#x2713; </Button>
+class orderSummary extends Component {
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return <li key={igKey} className="text-capitalize list">{igKey}: {this.props.ingredients[igKey]}</li>
+        });
+        return(
+            <Aux>
+                <h3>Your Order:</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul className="centre">
+                    {ingredientSummary}
+                </ul>
+                <p>Total Price: £{this.props.price.toFixed(2)}</p>
+                <p>Continue to checkout?</p>
+                <div className="row">
+                    <div className="col-md">
+                        <Button btnType="btn-danger" clicked={this.props.declinePurchase}> &#x2715; </Button>
+                    </div>
+                    <div className="col-md">
+                        <Button btnType="btn-success" clicked={this.props.acceptPurchase}> &#x2713; </Button>
+                    </div>
+                    
                 </div>
                 
-            </div>
-            
-        </Aux>
-    )
+            </Aux>
+        )
+    }
 }
 
 export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../../hoc/auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 import './Modal.scss';
 
-const modal = (props) => {
-    return(
-        <Aux>
-            <Backdrop show={props.show}  hideDisplay={props.modalClosed}/>
-            <div className={props.show ? "Modal modal-show" : 'Modal modal-hide'} >
-                {props.children}
-            </div>
-        </Aux>
-    )
+class modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.show !== this.props.show) {
+            return true;
+        }
+    }
+
+    render() {
+        return(
+            <Aux>
+                <Backdrop show={this.props.show}  hideDisplay={this.props.modalClosed}/>
+                <div className={this.props.show ? "Modal modal-show" : 'Modal modal-hide'} >
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
 }
 
 export default modal;
