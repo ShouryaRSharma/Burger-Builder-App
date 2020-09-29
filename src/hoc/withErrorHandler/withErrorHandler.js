51fbf4c7f8307d71e2116ab5899a6857e12fbc5e
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-09-29 14:54:25 +0100

    handling errors

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../auxiliary';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import './withErrorHandler.scss';
+
+const errorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        constructor() {
+            super();
+            this.state = ({
+                error: null
+            })
+        }
+
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({
+                    error: null
+                });
+                return req;
+            })
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({
+                    error: error
+                });
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({
+                error: null
+            })
+        }
+
+        render() {
+            const danger = <div className="Error"><FontAwesomeIcon size="70px" className="heartbeat" icon={faExclamationTriangle} />
+            <p className="danger-text mt-1">Something went wrong!</p></div>
+    
+            return(
+                <Aux>
+                    <Modal show={this.state.error} clicked={this.errorConfirmedHandler}>
+                        {this.state.error ? danger : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+    }
+}
+
+export default errorHandler;
