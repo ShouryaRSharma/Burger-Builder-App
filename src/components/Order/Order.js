bb89faec394937e84078d5bff57df4410ef67820
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-10-06 21:31:23 +0100

    Outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,12 +2,27 @@ import React from 'react'
 import classes from './Order.module.scss';
 
 const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName, 
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput= ingredients.map(ig => {
+        return (
+            <span className={classes.listText} key={ig.name}>{ig.name} ({ig.amount})</span>
+        );
+    });
+
     return(
         <div className={classes.order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>£ 5.45</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>£ {props.price}</strong></p>
         </div>
-    )
+    );
 }
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -24,6 +24,7 @@ class Orders extends Component {
                 loading: false,
                 orders: fetchedOrders
             });
+            
         }).catch(err => {
             this.setState({
                 loading: false,
@@ -32,10 +33,14 @@ class Orders extends Component {
     }
 
     render() {
+
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                    return(
+                        <Order key={order.id} ingredients={order.ingredients} price={order.price}/>
+                    );
+                })}
             </div>
         )
     }
